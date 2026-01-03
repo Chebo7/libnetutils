758b8731659c73d1ecaa7372d40c8f16842486e4
Chebo7 <192.cpp@gmail.com>
2026-01-03 22:49:56 +0300

    Added a function for obtaining IPv4 names of network interfaces

---
+++ src/Ipv4NetworkInterfaces.cpp
@@ -0,0 +1,19 @@
+#include "../include/netutils/Ipv4NetworkInterfaces.hpp"
+
+std::vector<std::string> NetUtils::Ipv4NetworkInterfaces() {
+  struct ifaddrs *ifaddr, *ifa;
+
+  if (getifaddrs(&ifaddr) == -1) {
+    throw "Unable to get network interfaces";
+  }
+
+  std::vector<std::string> interfaces;
+
+  for (ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+    if (ifa->ifa_addr != nullptr && ifa->ifa_addr->sa_family == AF_INET) {
+      interfaces.emplace_back(ifa->ifa_name);
+    }
+  }
+
+  return interfaces;
+}
