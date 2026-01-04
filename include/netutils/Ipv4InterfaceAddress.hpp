ae8540974901cec7bf51ea0353175b666a0a32bf
Chebo7 <192.cpp@gmail.com>
2026-01-04 18:21:05 +0300

    Added a function for obtaining an IP address by interface name.

---
+++ include/netutils/Ipv4InterfaceAddress.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <arpa/inet.h>
+#include <cstring>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <stdexcept>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+namespace NetUtils {
+std::string Ipv4InterfaceAddress(std::string interface);
+}
---
+++ src/Ipv4InterfaceAddress.cpp
@@ -0,0 +1,39 @@
+#include "../include/netutils/Ipv4InterfaceAddress.hpp"
+
+std::string NetUtils::Ipv4InterfaceAddress(std::string interface) {
+
+  struct ifaddrs *ifaddr, *ifa;
+
+  if (getifaddrs(&ifaddr) == -1) {
+    throw std::runtime_error("Unable to get network interfaces");
+  }
+
+  struct sockaddr_in *ipv4Addr = nullptr;
+  bool interfaceIsFound = false;
+
+  for (ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+    if (ifa->ifa_addr != nullptr && ifa->ifa_addr->sa_family == AF_INET &&
+        strcmp(ifa->ifa_name, interface.c_str()) == 0) {
+      ipv4Addr = (struct sockaddr_in *)ifa->ifa_addr;
+      interfaceIsFound = true;
+    }
+  }
+
+  if (!interfaceIsFound) {
+    freeifaddrs(ifaddr);
+    throw std::runtime_error("Interface not found");
+  }
+
+  if (ipv4Addr == nullptr) {
+    std::runtime_error("The interface has no address");
+  }
+
+  char ipStr[INET_ADDRSTRLEN];
+  inet_ntop(AF_INET, &(ipv4Addr->sin_addr), ipStr, INET_ADDRSTRLEN);
+
+  std::string address = ipStr;
+
+  freeifaddrs(ifaddr);
+
+  return address;
+}
