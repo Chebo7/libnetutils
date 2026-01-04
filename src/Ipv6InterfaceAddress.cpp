875a25ffdec7b44434503b5ac3cd5e63642d9bb5
Chebo7 <192.cpp@gmail.com>
2026-01-04 23:03:13 +0300

    Added a function for obtaining an IPv6 address by interface name.

---
+++ src/Ipv6InterfaceAddress.cpp
@@ -0,0 +1,39 @@
+#include "../include/netutils/Ipv6InterfaceAddress.hpp"
+
+std::string NetUtils::Ipv6InterfaceAddress(std::string interface) {
+
+  struct ifaddrs *ifaddr, *ifa;
+
+  if (getifaddrs(&ifaddr) == -1) {
+    throw std::runtime_error("Unable to get network interfaces");
+  }
+
+  struct sockaddr_in6 *ipv6Addr = nullptr;
+  bool interfaceIsFound = false;
+
+  for (ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+    if (ifa->ifa_addr != nullptr && ifa->ifa_addr->sa_family == AF_INET6 &&
+        strcmp(ifa->ifa_name, interface.c_str()) == 0) {
+      ipv6Addr = (struct sockaddr_in6 *)ifa->ifa_addr;
+      interfaceIsFound = true;
+    }
+  }
+
+  if (!interfaceIsFound) {
+    freeifaddrs(ifaddr);
+    throw std::runtime_error("Interface not found");
+  }
+
+  if (ipv6Addr == nullptr) {
+    std::runtime_error("The interface has no address");
+  }
+
+  char ipStr[INET6_ADDRSTRLEN];
+  inet_ntop(AF_INET6, &(ipv6Addr->sin6_addr), ipStr, INET6_ADDRSTRLEN);
+
+  std::string address = ipStr;
+
+  freeifaddrs(ifaddr);
+
+  return address;
+}
