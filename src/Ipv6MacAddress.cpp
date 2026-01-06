30d2f2a58a4bb58d738b24b76868877e208376aa
Chebo7 <192.cpp@gmail.com>
2026-01-06 18:13:34 +0300

    Added a function for obtaining the MAC address of the IPv6 interface.

---
+++ src/Ipv6MacAddress.cpp
@@ -0,0 +1,29 @@
+#include "../include/netutils/Ipv6MacAddress.hpp"
+
+std::string NetUtils::Ipv6MacAddress(const std::string interface) {
+  int sock = socket(AF_INET6, SOCK_DGRAM, 0);
+
+  if (sock == -1) {
+    throw std::runtime_error("Error to create socket");
+  }
+
+  struct ifreq ifr;
+  memset(&ifr, 0, sizeof(ifr));
+
+  strncpy(ifr.ifr_ifrn.ifrn_name, interface.c_str(), IFNAMSIZ);
+  ifr.ifr_ifrn.ifrn_name[IFNAMSIZ - 1] = '\0';
+
+  if (ioctl(sock, SIOCGIFHWADDR, &ifr) == -1) {
+    close(sock);
+    throw std::runtime_error("Error to get mac address");
+  }
+
+  close(sock);
+
+  unsigned char *mac =
+      reinterpret_cast<unsigned char *>(ifr.ifr_ifru.ifru_hwaddr.sa_data);
+
+  return std::string(std::format("{:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
+                                 mac[0], mac[1], mac[2], mac[3], mac[4],
+                                 mac[5]));
+}
