323d8e812c8b184daebabd263d4a0739841195a0
Chebo7 <192.cpp@gmail.com>
2026-01-11 22:01:11 +0300

    Added socket wrapper for RAII

---
+++ include/netutils/Socket/SmartSocket.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace NetUtils {
+namespace Socket {
+class SmartSocket {
+private:
+  int fd = 0;
+
+public:
+  SmartSocket(int domain, int type, int protocol);
+  SmartSocket(const SmartSocket &) = default;
+  SmartSocket(SmartSocket &&) = default;
+  SmartSocket &operator=(const SmartSocket &) = default;
+  SmartSocket &operator=(SmartSocket &&) = default;
+  ~SmartSocket();
+
+  int getFd() { return fd; }
+};
+} // namespace Socket
+} // namespace NetUtils
