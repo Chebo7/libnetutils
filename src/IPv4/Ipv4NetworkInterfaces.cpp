4474281de231777537f353b37689869fe3ccb3b0
Chebo7 <192.cpp@gmail.com>
2026-01-15 14:42:58 +0300

    Update error handling

--- src/IPv4/Ipv4NetworkInterfaces.cpp
+++ src/IPv4/Ipv4NetworkInterfaces.cpp
@@ -1,11 +1,37 @@
 #include "../../include/netutils/IPv4/Ipv4NetworkInterfaces.hpp"
+#include <cerrno>
+#include <format>
+#include <stdexcept>
+#include <string>
 
 std::vector<std::string> NetUtils::IPv4::Ipv4NetworkInterfaces() {
   struct ifaddrs *ifaddr, *ifa;
 
   if (getifaddrs(&ifaddr) == -1) {
     freeifaddrs(ifaddr);
-    throw "Unable to get network interfaces";
+    switch (errno) {
+    case ENOMEM:
+      throw std::runtime_error("Ipv4NetworkInterfaces: Cannot allocate memory");
+
+    case EFAULT:
+      throw std::runtime_error("Ipv4NetworkInterfaces: Bad address");
+
+    case EAFNOSUPPORT:
+      throw std::runtime_error(
+          "Ipv4NetworkInterfaces: Address family not supported by protocol");
+
+    case EINTR:
+      throw std::runtime_error(
+          "Ipv4NetworkInterfaces: Interrupted system call");
+
+    case ENFILE:
+      throw std::runtime_error(
+          "Ipv4NetworkInterfaces: Too many open files in system");
+
+    default:
+      throw std::runtime_error(
+          std::format("Ipv4NetworkInterfaces: {}", std::to_string(errno)));
+    }
   }
 
   std::vector<std::string> interfaces;
